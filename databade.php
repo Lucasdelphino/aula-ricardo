44b2cb5f7cfa6f9d26c322c491a0aa552a1aff52
Lucasdelphino <80285529+Lucasdelphino@users.noreply.github.com>
2023-10-26 16:15:53 -0300

    Initial commit

---
+++ databade.php
@@ -0,0 +1,12 @@
+ <?php 
+ $host "localhost";
+ $db_name = " php_beginner_crud_level_1 ";
+ $username = "root";
+ $password = "";
+ try{
+    $con = new PDO("mysql:host={$host};dbname={$db_name}", $username, $password);
+}
+catch(PDOException $expection){
+    echo "Connection erros: " . $expection->getMessage();
+}
+?>
